86003e17bf513a42b5bb79e9be030795b69f7a1a
Jon Dufty <jon.dufty95@gmail.com>
2021-07-26 21:59:06 +1000

    start react app

---
+++ app/src/AntLayout.js
@@ -0,0 +1,60 @@
+import { Layout, Menu, Breadcrumb } from 'antd';
+import React from 'react';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+class SiderDemo extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
+  render() {
+    const { collapsed } = this.state;
+    return (
+      <Layout className="site-layout">
+        <Layout style={{ minHeight: '100vh' }}>
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+            <div className="logo" />
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+              <Menu.Item key="1" icon={<PieChartOutlined />}>
+                Devices List
+              </Menu.Item>
+              <Menu.Item key="2" icon={<DesktopOutlined />}>
+                Config List
+              </Menu.Item>
+              <Menu.Item key="3" icon={<FileOutlined />}>
+                Monitor
+              </Menu.Item>
+            </Menu>
+          </Sider>
+        </Layout>
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+            Bill is not a cat.
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    );
+  }
+}
+
+export default SiderDemo;
---
+++ app/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { SideMenu } from './components/SideMenu';
+import "antd/dist/antd.css";
+import "./index.css";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+function App() {
+  return (
+    <Layout style = {{ minHeight: '100vh' }}>
+      <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Ben</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              Bill is a not cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+    </Layout>
+  );
+}
+
+export default App;
